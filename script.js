e7494492bbe5991eee3127f7a8e9327a310c661a
deanmih <153102912+deanmih@users.noreply.github.com>
2024-01-16 21:15:34 +0200

    script.js

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ let winLineExists = 0;
 function addChar(buttonId) {
     if (playerTurnIndex % 2 != 0) {
         document.getElementById(buttonId).innerHTML = "X";
+        document.getElementById(buttonId).disabled = true;
         ++playerTurnIndex;
     } else if (playerTurnIndex % 2 == 0) {
         document.getElementById(buttonId).innerHTML = "0";
+        document.getElementById(buttonId).disabled = true;
         ++playerTurnIndex;
     }
     for (let i = 0; winLineExists == 0 && i < winCombinations.length; ++i) {
@@ -22,9 +24,6 @@ function addChar(buttonId) {
         for (let j = 0; winLineExists == 1 && j < lineCheckPoints.length; ++j) {
             document.getElementById(lineCheckPoints[j]).style.background = "Black";
             document.getElementById(lineCheckPoints[j]).style.color = "White";
-        }
-        for (let k = 1; winLineExists == 1 && k <= 9 || playerTurnIndex == 10 && k <= 9; ++k) {
-            document.getElementById(k).disabled = true;
         }
         if (winLineExists == 1) {
             winMessage(firstPoint);
